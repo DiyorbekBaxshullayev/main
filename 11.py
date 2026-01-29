c7fcc1bb1e443a13a997c2232fd1b3ee7e4b616f
DiyotbekBaxshullayev <baxshullayevd4@gmail.com>
2026-01-29 14:58:39 +0500

    random

--- 11.py
+++ 11.py
@@ -1,2 +1,20 @@
-print('Hello World')
-print('Salom Dunyo')
+import random
+
+savollar_soni = 5
+togri_javoblar = 0
+
+for i in range(savollar_soni):
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
+
+    print(f"{i+1}-savol: {a} + {b} = ?")
+    javob = int(input("Javobingiz: "))
+
+    if javob == a + b:
+        print("To‘g‘ri! ✅\n")
+        togri_javoblar += 1
+    else:
+        print(f"Noto‘g‘ri ❌ To‘g‘ri javob: {a + b}\n")
+
+print("Test tugadi!")
+print(f"Siz {savollar_soni} ta savoldan {togri_javoblar} tasiga to‘g‘ri javob berdingiz.")
